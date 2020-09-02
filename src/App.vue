1a15b229211c11d2501c7ddde99b48d461c12dc1
chenbeile <lebeichen@qq.com>
2020-09-03 01:06:52 +0800

    init vite router

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <router-link to="/"> Frank</router-link>
+  <router-link to="/xxx"> Frank2</router-link>
+</div>
+<hr>
+<router-view />
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <div class="topnav">
+    <div class="logo"></div>
+    <div class="menu"></div>
+  </div>
+  <div class="banner">
+    <h1>轱辘ui</h1>
+    <h2>一个厉害的ui框架</h2>
+    <p class="actions">
+      <a href="">github</a>
+      <a href="">开始</a>
+    </p>
+  </div>
+</div>
+</template>
