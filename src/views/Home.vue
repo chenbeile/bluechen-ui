8a2dd42c317845168a0f42c8f1e36c16749fbb28
chenbeile <lebeichen@qq.com>
2020-09-04 00:48:27 +0800

    feat: topnav 样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
 <div>
   <div class="topnav">
-    <div class="logo"></div>
-    <div class="menu"></div>
+    <div class="logo">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
   </div>
   <div class="banner">
     <h1>轱辘ui</h1>
@@ -14,3 +17,26 @@
   </div>
 </div>
 </template>
+
+<style lang="scss" scoped>
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+
+  >.logo {
+    max-width: 6rem;
+    margin-right: auto;
+  }
+
+  >.menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+
+    >li {
+      margin: 0 1em;
+    }
+  }
+}
+</style>
