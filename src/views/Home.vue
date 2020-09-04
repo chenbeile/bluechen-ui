fc228dd4e1d1db05b8733baa636044485be528dc
chenbeile <lebeichen@qq.com>
2020-09-05 02:06:34 +0800

    feat: Home 页面的css优化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,23 +22,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.topnav {
-  background: pink;
+.banner {
+  padding: 100px 0;
   display: flex;
-  padding: 16px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
 
-  >.logo {
-    max-width: 6rem;
-    margin-right: auto;
-  }
+  >.actions {
+    padding: 8px 0;
 
-  >.menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-
-    >li {
-      margin: 0 1em;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
     }
   }
 }
